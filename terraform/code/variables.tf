0d32d98da168b76204f4bb0ee3ae49d58a039b10
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-04-23 20:52:03 -0600

    Add3

--- terraform/code/variables.tf
+++ terraform/code/variables.tf
@@ -33,17 +33,6 @@ variable "sg_name" {
   type        = string
 }
 
-variable "ecs_cluster_name" {
-  type        = string
-}
 
-
-variable "ecr_repository_name" {
-  type        = string
-}
-
-variable "ecs_service_name" {
-  type        = string
-}
 
 
