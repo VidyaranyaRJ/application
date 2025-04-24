7a9a0563fc9fb2fc9cc65042ffc2ae45ef11b6f3
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-04-24 17:02:18 -0600

    add day2-12

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -2,23 +2,6 @@ data "aws_iam_instance_profile" "ecs_profile" {
   name = "vj-ecs-ec2-1"
 }
 
-resource "tls_private_key" "vj_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "vj_ec2_key" {
-  key_name   = "vj-test"
-  public_key = tls_private_key.vj_key.public_key_openssh
-}
-
-resource "local_file" "vj_key_pem" {
-  content              = tls_private_key.vj_key.private_key_pem
-  filename             = "C:/Users/vrjav/Downloads/Desktop folders/apl_deployment/terraform/vj-test.pem"
-  file_permission      = "0400"
-  directory_permission = "0700"
-}
-
 
 resource "aws_instance" "ecs_instance" {
   ami                         = "ami-0c3b809fcf2445b6a"
@@ -27,7 +10,7 @@ resource "aws_instance" "ecs_instance" {
   vpc_security_group_ids      = [var.sg_id]
   iam_instance_profile        = data.aws_iam_instance_profile.ecs_profile.name
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.vj_ec2_key.key_name
+  key_name                    = "vj-test"
 
   tags = {
     Name = "ecs-instance-ubuntu"
