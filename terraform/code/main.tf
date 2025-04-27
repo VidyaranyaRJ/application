b83248d1bbc0fbcfb22886f1b40d11e45d96dcc1
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-04-26 20:07:54 -0600

    add day3-11

--- terraform/code/main.tf
+++ terraform/code/main.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
+
+
 terraform {
   backend "s3" {
     bucket         = "vj-test-ecr-79"  
