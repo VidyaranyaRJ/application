1a869a38f6b736f59d7060249706092b055f6088
Vidyaranya Javalagi <vrjavalagi779@gmail.com>
2025-05-10 16:13:07 -0600

    day1-11

--- terraform/code/variables.tf
+++ terraform/code/variables.tf
@@ -10,13 +10,13 @@ variable "instance_type" {
   type        = string
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type        = string
-}
+# variable "AWS_ACCESS_KEY_ID" {
+#   type        = string
+# }
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  type        = string
-}
+# variable "AWS_SECRET_ACCESS_KEY" {
+#   type        = string
+# }
 
 
 
